64e7e380541bc61ed3b066a6be42a799262b40dc
lanm <markus.langenegger@kilchenmann.ch>
2023-11-26 09:48:06 +0100

    get webunits classes and import only active classes

--- src/models/student-class/student-class.service.ts
+++ src/models/student-class/student-class.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, OnModuleInit} from '@nestjs/common';
 import { CreateStudentClassDto } from './dto/create-student-class.dto';
 import { UpdateStudentClassDto } from './dto/update-student-class.dto';
 import {InjectRepository} from "@nestjs/typeorm";
@@ -7,17 +7,21 @@ import {GraphApiService} from "../../providers/graph-api.service";
 import {UserAccessService} from "../../providers/user-access.service";
 import {StudentClass} from "./entities/student-class.entity";
 import {AppCustomLogger} from "../../app.custom.logger";
+import { WebUntisAnonymousAuth } from 'webuntis';
 
 @Injectable()
 export class StudentClassService {
   private readonly logger = new AppCustomLogger(StudentClassService.name);
-
+  private untis = new WebUntisAnonymousAuth('hftm', 'mese.webuntis.com')
+  private webUntisClasses = []
   constructor(
       @InjectRepository(StudentClass)
       private classRepository: Repository<StudentClass>,
       private graphApiService: GraphApiService,
       private userAccessService: UserAccessService,
-  ) {}
+  ) {
+
+  }
   create(createStudentClassDto: CreateStudentClassDto) {
     return this.classRepository.save(createStudentClassDto);
   }
@@ -39,21 +43,20 @@ export class StudentClassService {
   async createClasses() {
     let message = {message: ``, status: 500};
     const token = await this.userAccessService.getAccessToken();
-    await this.graphApiService.getClasses(token).then(async (studentClass: any[]) => {
+    const  webUntisClasses = await this.getCurrentWebUntisClasses()
+    this.logger.log(`Current count of webuntis classes  ${webUntisClasses.length}`)
+    await this.graphApiService.getClasses(token).then(async (studentClasses: any[]) => {
       let count = 0;
-      for (const classes of studentClass) {
-        if (classes?.groupTypes.length == 0 && classes?.onPremisesDomainName === "hftm.ch" && classes?.mail !== null) {
-
-          if(classes?.displayName === "Geschäftsleitung" || classes?.displayName === "Dozierende" || classes?.displayName === "BBDA18.3a"){
-            console.log(classes)
-          }
+      console.log(webUntisClasses)
+      for (const studentClass of studentClasses) {
+        if (this.compareObjectsInArray(webUntisClasses, studentClass)) {
           const currentClass = await this.classRepository.findOne({
-            where: {oid: classes.id},
+            where: {oid: studentClass.id},
           });
           if (currentClass === null) {
             const classEntity = new StudentClass();
-            classEntity.oid = classes.id;
-            classEntity.name = classes.displayName;
+            classEntity.oid = studentClass.id;
+            classEntity.name = studentClass.displayName;
             await this.create(classEntity);
             count++;
            }
@@ -66,5 +69,23 @@ export class StudentClassService {
           message = { message: `Unauthorized`, status: 401 };
         });
     return message;
+  }
+
+  private compareObjectsInArray(array1, object2) {
+    for (let i = 0; i < array1.length; i++) {
+      const currentObject = array1[i];
+      if (currentObject.name && object2.displayName) {
+        if (currentObject.name === object2.displayName) {
+          return true; // Return true if any object matches the condition
+        }
+      }
+    }
+    return false; // Return false if no match is found
+  }
+
+  private async getCurrentWebUntisClasses(){
+    await this.untis.login()
+    const schoolYear = await this.untis.getSchoolyears()
+    return await this.untis.getClasses(true, schoolYear[0].id)
   }
 }
