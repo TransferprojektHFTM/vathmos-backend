06a8b8499847bee766fa72c95bcb92c7c15e8338
= <kilualthaus@hotmail.com>
2023-12-17 10:18:23 +0100

    update.dto extend creat.dto exam

--- src/models/exam/dto/update-exam.dto.ts
+++ src/models/exam/dto/update-exam.dto.ts
@@ -1,18 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { Subject } from '../../subject/entities/subject.entity';
-import { IsNotEmpty } from 'class-validator';
+import { CreateExamDto } from './create-exam.dto';
 
-export class UpdateExamDto {
-
-    @IsNotEmpty()
-    @ApiProperty({ example: 'Verteilte Systeme Test 2' })
-    name: string;
-
-    @IsNotEmpty()
-    @ApiProperty({ example: '40%' })
-    weighting: string;
-
-    @IsNotEmpty()
-    @ApiProperty({ example: 1 })
-    subject: Subject;
+export class UpdateExamDto extends CreateExamDto {
 }
