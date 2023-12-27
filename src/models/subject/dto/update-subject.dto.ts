bc7fb8b313379cbd090c3b862579750daa67b113
= <kilualthaus@hotmail.com>
2023-12-27 20:09:52 +0100

    partialtype update subject dto

--- src/models/subject/dto/update-subject.dto.ts
+++ src/models/subject/dto/update-subject.dto.ts
@@ -1,3 +1,5 @@
-import { CreateSubjectDto } from './create-subject.dto';
+import {CreateSubjectDto} from './create-subject.dto';
+import {PartialType} from '@nestjs/swagger';
 
-export class UpdateSubjectDto extends CreateSubjectDto {}
+export class UpdateSubjectDto extends PartialType(CreateSubjectDto) {
+}
